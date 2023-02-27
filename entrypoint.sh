d4eff202c22bca422afdf335e9d18187662bd6f2
Elio Hoyeck <79227981+Eliohoyeck@users.noreply.github.com>
2023-02-27 10:23:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -381,5 +381,6 @@ git config --local user.name "${GITHUB_ACTOR}"
 git config --local user.email "${GITHUB_ACTOR}@users.noreply.github.com"  
 git add modules-versions.md
 git commit -m "Append outputs to modules-versions.md"
-git push origin main
+git push https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git HEAD:main
+
 
